a9ab35f728e743d94c0ee8d60210f2810a9849eb
y-taki <yusuke.taki@proaxia-consulting.co.jp>
2021-10-18 16:07:56 +0900

    mod to run UnitTest as build

--- blackJack/UnitTest.sh
+++ blackJack/UnitTest.sh
@@ -1,2 +1,29 @@
-echo "test"
-exit 1
+target="blackJack"
+destination="platform=macOS, name=Any Mac"
+
+start_time=`date +%s`
+
+schemaList=(`xcodebuild -list | sed -n '/Scheme/,$p' | tail -n +2`)
+failedSchemaList=()
+succeededSchemaList=()
+for scheme in "${schemaList[@]}" ;do
+    echo "TestStart ${scheme}"
+    xcodebuild  -scheme $scheme -target $target  build
+    if [ $? -ne 0 ]; then
+      failedSchemaList+="$scheme\n"
+    else
+      succeededSchemaList+="$scheme\n"
+    fi
+done
+
+echo "テストが失敗したPackage"
+echo $failedSchemaList
+
+echo "テストが成功したPackage"
+echo $succeededSchemaList
+
+end_time=`date +%s`
+
+
+run_time=$((end_time - start_time))
+echo $run_time
